374fe29c606d910236e294b391bbc2e8f1a5a0b0
chiangryanNFU <230967201+chiangryanNFU@users.noreply.github.com>
2025-11-10 16:14:21 +0800

    Created 2 MainWindow

--- main.cpp
+++ main.cpp
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    MainWindow w2;
+    w.resize(600,400);
+    w2.resize(600,400);
+    w.move(300,300);
+    w2.move(900,300);
     w.show();
+    w2.show();
     return a.exec();
 }
